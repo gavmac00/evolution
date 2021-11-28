e9aedaabcf30ceccc237468ccd14beccdd116a5c
gavmac00 <macdonga@tcd.ie>
2021-11-28 19:18:13 +0000

    main

--- evolute.py
+++ evolute.py
@@ -39,18 +39,48 @@ class NewWorld:
             Lute.storeLifeStatus(l)
             count = count + 1
 
-NewWorld.spawn()
+class World:
+    def __init__(W, ID):
+        W.ID = ID
+    
+    def living():
+        alive = 0
+        for l in worldLifeStatus:
+            if worldLifeStatus[l] == 1:
+                alive +=1
+        print(f"\nCurrently Living: {alive}/{startingPopulation} lutes.")
 
-# print(f"\nAll cells occupied in X: {occupiedX}")
-# print(f"All cells occupied in Y: {occupiedY}")
+    def kill(ID):
+        del(occupiedX[ID-1])
+        del(occupiedY[ID-1])
+        worldLifeStatus[ID-1] = 0
+
+NewWorld.spawn()
+World.living()
+
+print(f"\nAll cells occupied in X: {occupiedX}")
+print(f"All cells occupied in Y: {occupiedY}")
 print(f"\nWorldsize: {worldSize} x {worldSize}.")
 print(f"The starting population is {startingPopulation}.")
 print(f"In the population, the status of life is {worldLifeStatus}.\n")
 
-# print(f"There will be {genAmount} generations.\n")
+print(f"There will be {genAmount} generations.\n")
 
-# count = 1
-# while count < 6:
-#     ID = input(f"Query a Lute number's coordiantes: (Enter a number): ")
-#     ID = int(ID)
-#     print(f"({occupiedX[ID-1]},{occupiedY[ID-1]})")
+locationRequest = input(f"Want to query a lutes location? (Y/N): ")
+if locationRequest == "Y":
+    count = 0
+    while count < startingPopulation:
+        ID = input(f"Query a Lute number's coordiantes: (Enter a number between 1 and {startingPopulation}): ")
+        ID = int(ID)
+        print(f"({occupiedX[ID-1]},{occupiedY[ID-1]})")
+        count += 1
+
+killRequest = input(f"Want to kill any? (Y/N): ")
+if killRequest == "Y":
+    ID = input(f"Enter the Lutes ID (Between 1 and {startingPopulation}): ")
+    ID = int(ID)
+    World.kill(ID)
+    print(f"\nAll cells occupied in X: {occupiedX}")
+    print(f"All cells occupied in Y: {occupiedY}")
+    print(f"In the population, the status of life is {worldLifeStatus}.\n")
+    World.living()
